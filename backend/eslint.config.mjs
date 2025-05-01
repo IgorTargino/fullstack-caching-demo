da6bb0c12220ce08612a56e134f552ef98563372
igortargino <igortargino01@gmail.com>
2025-05-01 13:39:27 -0300

    config: update eslint rules

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,16 +1,30 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import unusedImportsPlugin from 'eslint-plugin-unused-imports';
+import promisePlugin from 'eslint-plugin-promise';
+import jestPlugin from 'eslint-plugin-jest';
 import tseslint from 'typescript-eslint';
+import * as importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'coverage', 'node_modules'],
+    files: ['**/*.{js,mjs,cjs,ts}'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+    },
+  },
   {
     languageOptions: {
       globals: {
@@ -20,15 +34,33 @@ export default tseslint.config(
       sourceType: 'commonjs',
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        tsconfigRootDir: new URL('.', import.meta.url).pathname,
       },
     },
   },
   {
+    plugins: { 'unused-imports': unusedImportsPlugin },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
+  promisePlugin.configs['flat/recommended'],
+  importPlugin.flatConfigs?.recommended,
+  importPlugin.flatConfigs?.typeScript,
+  {
+    settings: {
+      'import/resolver': { typescript: true, node: true },
+    },
+  },
+
+  jestPlugin.configs['flat/recommended'],
 );
